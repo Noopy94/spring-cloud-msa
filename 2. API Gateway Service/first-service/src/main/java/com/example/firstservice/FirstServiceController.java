f2890a79d09a0bbb1a7e3136d3f78aa4174704b8
이두현 <ldh9411@hanmail.net>
2023-10-16 17:57:43 +0900

    Feat: 유저 서비스 추가
    - UserService 추가

--- 2. API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ 2. API Gateway Service/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,7 +1,11 @@
 package com.example.firstservice;
 
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service/")
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env){
+        this.env = env;
+    }
 
 
     @GetMapping("/welcome")
@@ -26,8 +37,10 @@ public class FirstServiceController {
 
 
     @GetMapping("/check")
-    public String check() {
-        return "Check, Hello World in First Service";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Check, Hello World in First Service on Port %s", env.getProperty("local.server.port"));
     }
 
 
